c2fea7ac440d30559e70ad0348f5537b18f2ae45
Leandro Lemos <leandro.lemos@hothouse.com.au>
2015-02-09 18:47:17 +1100

    Removing unnecessary files

--- src/dimensions.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	outerWidth: (el) => {
-		var width = el.offsetWidth;
-		var style = getComputedStyle(el);
-
-		width += parseInt(style.marginLeft) + parseInt(style.marginRight);
-		return width;
-	}	
-} 
