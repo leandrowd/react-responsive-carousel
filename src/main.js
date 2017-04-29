5d5b7122c6c6500c584d1370433bd15b46b67038
Leandro Lemos <llemos@atlassian.com>
2017-04-29 18:35:23 +1000

    #112: Fix emulateTouch

--- src/main.js
+++ src/main.js
@@ -1,51 +1,55 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Carousel = require('./components/Carousel');
-var CreateReactClass = require('create-react-class');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Carousel from './components/Carousel';
 
-function onChange() {
+function _onChange() {
     console.log('onChange', arguments);
 }
 
-function onClickItem() {
+function _onClickItem() {
     console.log('onClickItem', arguments);
 }
 
-function onClickThumb() {
+function _onClickThumb() {
     console.log('onClickThumb', arguments);
 }
 
 // Begin DemoSliderControls
-var DemoCarousel = CreateReactClass({
-    render() {
-        return (
-            <Carousel showArrows infiniteLoop autoPlay emulateTouch onChange={onChange} onClickItem={onClickItem} onClickThumb={onClickThumb}>
-                <div>
-                    <img src="assets/1.jpeg" />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src="assets/2.jpeg" />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src="assets/3.jpeg" />
-                    <p className="legend">Legend 3</p>
-                </div>
-                <div>
-                    <img src="assets/4.jpeg" />
-                    <p className="legend">Legend 4</p>
-                </div>
-                <div>
-                    <img src="assets/5.jpeg" />
-                    <p className="legend">Legend 5</p>
-                </div>
-                <div>
-                    <img src="assets/6.jpeg" />
-                    <p className="legend">Legend 6</p>
-                </div>
-            </Carousel>
-        );
-    }
-});
+const DemoCarousel = () => (
+    <Carousel
+        showArrows
+        infiniteLoop
+        autoPlay
+        emulateTouch
+        onClickItem={_onClickItem}
+        onChange={_onChange}
+        onClickThumb={_onClickThumb}
+    >
+        <div>
+            <img src="assets/1.jpeg" />
+            <p className="legend">Legend 1</p>
+        </div>
+        <div>
+            <img src="assets/2.jpeg" />
+            <p className="legend">Legend 2</p>
+        </div>
+        <div>
+            <img src="assets/3.jpeg" />
+            <p className="legend">Legend 3</p>
+        </div>
+        <div>
+            <img src="assets/4.jpeg" />
+            <p className="legend">Legend 4</p>
+        </div>
+        <div>
+            <img src="assets/5.jpeg" />
+            <p className="legend">Legend 5</p>
+        </div>
+        <div>
+            <img src="assets/6.jpeg" />
+            <p className="legend">Legend 6</p>
+        </div>
+    </Carousel>
+);
+
 ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
