285443890b35e87a7ee3c8dc2ad22447085a1f85
Leandro Lemos <leandrowd@gmail.com>
2020-04-11 23:57:51 +1000

    gh-422: Use node 10

--- tasks/runShellCommand.js
+++ tasks/runShellCommand.js
@@ -1,8 +1,8 @@
 const { spawn } = require('child_process');
 
-module.exports = function (shellCommand, args, callback) {
+module.exports = function(shellCommand, args, callback) {
     const command = spawn(shellCommand, args);
-    
+
     command.on('close', (code) => {
         if (code !== 0) {
             console.log(`command "${shellCommand}" exited with code ${code}`);
