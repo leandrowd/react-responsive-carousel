15c24dca3b6bc2076b0398533ab0c52aa49ccb60
Leandro Lemos <leandrowd@gmail.com>
2020-04-12 14:12:54 +1000

    gh-422: Include codesandbox  in the repo to allow auto update

---
+++ codesandbox/default/src/Carousel.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+
+export default () => (
+    <Carousel autoPlay>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
+            <p className="legend">Legend 1</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" />
+            <p className="legend">Legend 2</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" />
+            <p className="legend">Legend 3</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" />
+            <p className="legend">Legend 4</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-5.jpg" />
+            <p className="legend">Legend 5</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-6.jpg" />
+            <p className="legend">Legend 6</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-7.jpg" />
+            <p className="legend">Legend 7</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-8.jpg" />
+            <p className="legend">Legend 8</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-9.jpg" />
+            <p className="legend">Legend 9</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-10.jpg" />
+            <p className="legend">Legend 10</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-11.jpg" />
+            <p className="legend">Legend 11</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-12.jpg" />
+            <p className="legend">Legend 12</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-13.jpg" />
+            <p className="legend">Legend 13</p>
+        </div>
+        <div>
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-14.jpg" />
+            <p className="legend">Legend 14</p>
+        </div>
+    </Carousel>
+);
