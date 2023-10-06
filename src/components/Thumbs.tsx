e3f0b41b6d7b9aa75088065343cc443bc2fac351
Vinh Nguyen <nguyenbavinhcntt@gmail.com>
2023-10-06 14:29:07 +0700

    Update thumb wrapper size calculation

--- src/components/Thumbs.tsx
+++ src/components/Thumbs.tsx
@@ -188,7 +188,7 @@ export default class Thumbs extends Component<Props, State> {
             deltaX = 0;
         }
 
-        const wrapperSize = this.itemsWrapperRef.clientWidth;
+        const wrapperSize = this.itemsWrapperRef.clientWidth - ( 6 * childrenLength );
         const position = currentPosition + 100 / (wrapperSize / deltaX);
         // if 3d isn't available we will use left to move
         if (this.itemsListRef) {
